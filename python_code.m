3355d831f968617ffe8c15472d6fa6a5ce8ac15c
Quincys03 <111908493+Quincys03@users.noreply.github.com>
2024-11-05 14:35:20 -0700

    Add files via upload

---
+++ python_code.m
@@ -0,0 +1,12 @@
+
+
+mylist = py.list({'apple', 'banana', 'orange'});
+py_math = py.importlib.import_module('math');
+result = py_math.sqrt(25);
+disp(result);
+
+my_str = 'Hello MATLAB';
+
+reversed_str = py.str(my_str(end:-1:1));
+
+disp(reversed_str);
